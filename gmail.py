30bf114e1382575979181cf53402a88554576469
munjamSaipriya21 <90600120+munjamSaipriya21@users.noreply.github.com>
2022-01-06 20:23:32 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,13 @@
+gmail= ["priya priya@gmail.com",
+    "julia julia@julia.me",
+    "sai sai@gmail.com",
+    "julia julia@gmail.com",
+    "samantha samantha@gmail.com",
+    "saipriya saipriya@gmail.com"]
+
+for val in gmail:
+    # print(val)
+    spl=val.split()
+    print(spl[0])
+
+
